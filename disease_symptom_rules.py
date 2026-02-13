6e2ab7471b2d1bd39699743d8b65b0582609f913
Prerna Malhotra <prernamalhotra997@gmail.com>
2026-02-13 22:55:11 +0530

    Add files via upload

---
+++ disease_symptom_rules.py
@@ -0,0 +1,13 @@
+# disease_symptom_rules.py
+
+DISEASE_SYMPTOM_MAP = {
+    "dengue": ["fever", "rash", "headache", "fatigue", "body pain", "red spots"],
+    "measles": ["fever", "rash", "cough", "red spots", "runny nose"],
+    "chicken pox": ["itching", "rash", "red spots", "fever", "body pain"],
+    "fungal infection": ["itching", "skin rash", "red patches"],
+    "asthma": ["cough", "breathing difficulty", "wheezing"],
+    "jaundice": ["yellow skin", "yellow eyes", "fatigue", "nausea"],
+    "covid": ["fever", "cough", "fatigue", "sore throat"],
+    "uti": ["burning urination", "pelvic pain", "frequent urination"],
+    # add more over time
+}
